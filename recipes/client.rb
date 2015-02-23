b49fecaca7fde53be83125e8c39bd62458253250
unknown <Aleksej_Barkhanov@EPBYMINW4780.minsk.epam.com>
2015-02-23 15:09:52 +0300

    nfs client configuration

--- recipes/client.rb
+++ recipes/client.rb
@@ -7,4 +7,11 @@
 # All rights reserved - Do Not Redistribute
 #
 
-include_recipe "nfs::client"
+include_recipe "nfs::client4"
+
+mount "/var/www/wordpress" do
+  device node["wp"]["nfs"]
+  mount_point "/var/www/wordpress"
+  fstype "ext4"
+  action :mount
+end
