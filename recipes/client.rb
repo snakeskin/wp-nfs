e4ecbca161aed5a36cba5900eff7cedc71ab4258
unknown <Aleksej_Barkhanov@EPBYMINW4780.minsk.epam.com>
2015-02-23 15:13:56 +0300

    nfs mount configurations

--- recipes/client.rb
+++ recipes/client.rb
@@ -10,8 +10,7 @@
 include_recipe "nfs::client4"
 
 mount "/var/www/wordpress" do
-  device node["wp"]["nfs"]
-  mount_point "/var/www/wordpress"
-  fstype "ext4"
-  action :mount
+  device "node["wp"]["nfs"]:/var/www/wordpress"
+  fstype "nfs"
+  options "rw"
 end
