133e3e6ba10643ff3cccd4523247db5085a1d154
unknown <Aleksej_Barkhanov@EPBYMINW4780.minsk.epam.com>
2015-03-03 14:07:15 +0300

    1

--- recipes/server.rb
+++ recipes/server.rb
@@ -7,11 +7,11 @@
 # All rights reserved - Do Not Redistribute
 #
 
-include_recipe "nfs::server"
+include_recipe "nfs::server4"
 
 nfs_export "/var/www/wordpress" do
   network '192.168.50.0/24'
-  writeable false 
+  writeable true 
   sync true
   options ['no_root_squash']
 end
