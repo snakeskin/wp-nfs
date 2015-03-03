754a0d0bce2d17ff97efba7142102c01d5df4ca9
unknown <Aleksej_Barkhanov@EPBYMINW4780.minsk.epam.com>
2015-03-03 14:46:51 +0300

    1

--- recipes/server.rb
+++ recipes/server.rb
@@ -11,6 +11,8 @@ include_recipe "nfs::server4"
 
 nfs_export "/var/www/wordpress" do
   network '192.168.50.0/24'
+  anonuser "root"
+  anongroup "root"
   writeable true 
   sync true
   options ['no_root_squash']
